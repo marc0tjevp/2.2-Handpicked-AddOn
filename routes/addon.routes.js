66ba309a17268ef8572fa39e40689260c0a7de05
Marco van Poortvliet <Marc0tjevp@gmail.com>
2019-01-10 11:17:52 +0100

    Mock get function for contacts

--- routes/addon.routes.js
+++
@@ -1,6 +0,0 @@
-let routes = require('express').Router()
-let controller = require('../controllers/addon.controller')
-
-routes.get('/:email', controller.get)
-
-module.exports = routes
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 let routes = require('express').Router()
-let example_routes = require('./addon.routes')
+let example_routes = require('./contacts.routes')
 
 routes.get('/', (req, res) => res.status(200).json({"messsage": "Hello World!"}))
-routes.use('/company', example_routes)
+routes.use('/contacts', example_routes)
 routes.use('*', (req, res) => res.status(404).json({"message": "Not found"}).end())
 
 module.exports = routes
